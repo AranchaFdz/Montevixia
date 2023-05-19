b790832ba55167b057c17fdd792f8a34f32c6010
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-05-20 00:52:34 +0200

    Template and dashboard

---
+++ Config/config.php
@@ -0,0 +1,22 @@
+<?php 
+	const BASE_URL = "http://localhost/Montevixia";
+
+	date_default_timezone_set('Europa/Spain');
+
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_montevixia";
+	const DB_USER = "root";
+	const DB_PASSWORD = "Aussie1005.";
+	const DB_CHARSET = "utf8";
+
+	const SPD = ".";
+	const SPM = ",";
+
+	const SMONEY = "€";
+
+	const NOMBRE_REMITENTE = "Blog Montevixia";
+	const EMAIL_REMITENTE = "no-reply@empresa.com";
+	const NOMBRE_EMPESA = "Blog Montevixia";
+	const WEB_EMPRESA = "Página Web empresa";
+
+?>
